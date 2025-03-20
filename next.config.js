60ec7fe059119c00b8bea6a2157f03193d10a93f
faisalanshory <faisalanshory.m@gmail.com>
2025-03-20 22:02:51 +0700

    Setup project for Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      // Tambahkan domain untuk gambar eksternal jika ada
+    ],
+  },
+  // Tambahkan konfigurasi lain jika diperlukan
+}
+
+module.exports = nextConfig 
